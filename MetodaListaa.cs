c4e6b4b58c7ee1ba03f1f0b8b90b2c55e83a534f
Jakub <92061403+Oblyjodak@users.noreply.github.com>
2021-11-30 12:26:01 +0100

    Add files via upload

---
+++ MetodaListaa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MetodaListaa
+    {
+        public float a, b, c;
+        public List<float> PoczatkowaLista = new List<float> { };
+
+        public List<float> SortowanieListy(List<float> ToSort)
+        {
+            return new List<float>() { ToSort.Min(), ToSort.Max(), ToSort.Average() };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetodaListaa metodaLista = new MetodaListaa();
+
+            WartoscPodana(metodaLista);
+
+            metodaLista.SortowanieListy(metodaLista.PoczatkowaLista);
+
+            Wyswietl(metodaLista);
+
+            Console.ReadKey();
+        }
+
+        static void WartoscPodana(MetodaListaa obdzekt)
+        {
+            obdzekt.a = float.Parse(Console.ReadLine());
+            obdzekt.PoczatkowaLista.Add(obdzekt.a);
+            obdzekt.b = float.Parse(Console.ReadLine());
+            obdzekt.PoczatkowaLista.Add(obdzekt.b);
+            obdzekt.c = float.Parse(Console.ReadLine());
+            obdzekt.PoczatkowaLista.Add(obdzekt.c);
+        }
+
+        static void Wyswietl(MetodaListaa obdzekt)
+        {
+            List<float> wyswietlanie = obdzekt.SortowanieListy(obdzekt.PoczatkowaLista);
+            Console.WriteLine("Min: " + wyswietlanie[0]);
+            Console.WriteLine("Max: " + wyswietlanie[1]);
+            Console.WriteLine("Avg: " + wyswietlanie[2]);
+        }
+    }
+}
